16a0f6b8085c9cb24d6e217d18b06d59af2a5d33
neto <teixeira.neto19981@gmail.com>
2025-06-15 11:18:52 -0300

    11.9

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ require("dotenv").config();
 
 const app = express();
 const server = require('http').createServer(app);
-//const io = require('socket.io')(server ,{origin : '*',maxHttpBufferSize: 1e8, pingTimeout: 60000,});
+const io = require('socket.io')(server ,{origin : '*',maxHttpBufferSize: 1e8, pingTimeout: 60000,});
 const readable = require('stream').Readable;
 const fs = require("fs");
 
@@ -34,12 +34,17 @@ console.log(fs.existsSync('/socket.io'));
 var users_connected = [];
 const message_types = ["TEXT","BASE64"];
 
+app.use(path.join(process.cwd(), 'public'), function(req, res, next){
+  req.method = 'GET';
+  next();
+});
+
 app.use(express.static(path.join(process.cwd(), 'public'), {index:'index.html'}));
 app.set('views', path.join(process.cwd(), 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html'); 
 
-/*io.on('connection', socket => {
+io.on('connection', socket => {
 
     console.log("bla");
 
@@ -303,7 +308,7 @@ app.set('view engine', 'html');
      * /////////////////////////////////////////////////////////////////////////////////////
      * ---------///////////---------Recebe arquivos de base64----------//////////////------
      * ////////////////////////////////////////////////////////////////////////////////////
-     *
+     */
     socket.on('base64', data => {
         //console.log(data.user);
         //Cria um nome aleatório
